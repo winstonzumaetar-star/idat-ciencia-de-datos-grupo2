64ae885072874247691a00a7ecc9f242ce35ef87
Henry Vega <hp.vega@hotmail.com>
2026-03-12 21:03:17 -0500

    Add CSV files and Python script for data aggregation and analysis

--- sesion-4/agrupador/agrupador.py
+++ sesion-4/agrupador/agrupador.py
@@ -7,4 +7,8 @@ resultado = pd.merge(facturacion, clientes, on="id_cliente", how="left")
 
 agrupador = resultado.groupby(["nombre", "pais"]).sum()["monto"]
 
-print(agrupador)
+# Exporta en csv
+agrupador.to_csv("Agrupados.csv")
+
+# Exporta en xslx usando pip install openpyxl
+agrupador.to_excel("Agrupados.xlsx")
